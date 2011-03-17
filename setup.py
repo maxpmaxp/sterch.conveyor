840e2e1c53a3e3d180302141e5e47bd89910b425
Maxim Polscha <maxp@sterch.net>
2011-03-17 15:00:26 +0200

    version has been changed to 0.1

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ def alltests():
     return suite
 
 setup( name='sterch.conveyor',
-    version='0.1a',
+    version='0.1',
     url='http://pypi.sterch.net/sterch.conveyor',
     license='ZPL 2.1',
     description='Provides ZCML-based tools to define simultaneous tasks processing conveyors',
